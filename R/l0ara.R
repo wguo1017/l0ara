4319eb0dbe95178f8af80ab21551c062180c0851
wguo1990 <Wenchuan@Wenchuans-MacBook-Pro-2.local>
2016-08-04 16:59:00 -0700

    Added details

--- R/l0ara.R
+++ R/l0ara.R
@@ -4,10 +4,10 @@
 #' @param x Input matrix, of dimension nobs x nvars; each row is an observation vector.
 #' @param y Response variable. Quantitative for \code{family="gaussian"}; positive quantitative for \code{family="gamma"} or \code{family="inv.gaussian"} ; a factor with two levels for \code{family="logit"}; non-negative counts for \code{family="poisson"}.
 #' @param family Response type(see above).
-#' @param lam A user supplied \code{lambda} value. Try use \code{cv.l0ara} first to select optimal tunning and then refit with \code{lam.min}.
+#' @param lam A user supplied \code{lambda} value. Try use \code{cv.l0ara} first to select optimal tunning and then refit with \code{lam.min} if you have a \code{lam} sequence. To use AIC, set \code{lam=2}; to use BIC, set \code{lam=log(n)}.
 #' @param maxit Maximum number of passes over the data for \code{lambda}. Default value is \code{1e3}.
 #' @param eps Convergence threshold. Default value is \code{1e-4}.
-#' @details The sequence of models indexed by the parameter lambda is fit using adptive ridge algorithm. The objective function for generalized linear models (including \code{family} above) is defined to be \deqn{-(log likelihood)+(\lambda/2)*|\beta|_0} This adaptive ridge algorithm is developed to approximate L0 penalized generalized linear models with sequential optimization and is efficient for high-dimensional data.
+#' @details The sequence of models indexed by the parameter lambda is fit using adptive ridge algorithm. The objective function for generalized linear models (including \code{family} above) is defined to be \deqn{-(log likelihood)+(\lambda/2)*|\beta|_0} \eqn{|\beta|_0} is the number of non-zero elements in \eqn{\beta}. To select the "best" model with AIC or BIC criterion, let \code{lambda} to be 2 or \code{log(n)}. This adaptive ridge algorithm is developed to approximate L0 penalized generalized linear models with sequential optimization and is efficient for high-dimensional data.
 #' @return An object with S3 class "l0ara" containing:
 #'  \item{beta}{A vector of coefficients}
 #'  \item{df}{Number of nonzero coefficients}
@@ -31,8 +31,8 @@
 #' beta <- c(1,0,2,3,rep(0,p-4))
 #' noise <- rnorm(n)
 #' y <- x%*%beta+noise
-#' # fit sparse linear regression
-#' res.gaussian <- l0ara(x, y, family="gaussian", 1)
+#' # fit sparse linear regression using BIC 
+#' res.gaussian <- l0ara(x, y, family="gaussian", log(n))
 #'
 #' # predict for new observations
 #' print(res.gaussian)
@@ -63,8 +63,8 @@
 #' beta <- c(1,0,0.5,0.3,rep(0,p-4))
 #' mu <- exp(x%*%beta)
 #' y <- rpois(n, mu)
-#' # fit sparse Poisson regression
-#' res.pois <- l0ara(x, y, family="poisson", 0.7)
+#' # fit sparse Poisson regression using AIC
+#' res.pois <- l0ara(x, y, family="poisson", 2)
 #'
 #' # predict for new observations
 #' print(res.pois)
@@ -104,39 +104,7 @@ l0ara <- function(x, y, family = c("gaussian", "logit", "gamma", "poisson", "inv
   if (length(y)!=np[1]){
     stop("length of y not equal to the number of rows of x")
   }
-
-#   # standardize
-#   if (standardize) {
-#     xx <- scale(x)
-#     center <- colMeans(x)
-#     scale <- apply(x,2,sd)
-#     nd <- which(scale > 1e-6)
-#     if (length(nd) != np[2]) xx <- xx[ ,nd, drop=FALSE]
-#   } else {
-#     xx <- x
-#   }
-#   if (family=="gaussian") {
-#     yy <- y - mean(y)
-#   } else {
-#     yy <- y
-#   }
-
-#   # fitting model
-#   if (family == "gaussian") {
-#     out <- gaussl0(x, y, lam, maxit, eps)
-#   }
-#   if (family == "gamma") {
-#     out <- gammal0(x, y, lam, maxit, eps)
-#   }
-#   if (family == "inv.gaussian") {
-#     out <- invl0(x, y, lam, maxit, eps)
-#   }
-#   if (family == "logit") {
-#     out <- logitl0(x, y, lam, maxit, eps)
-#   }
-#   if (family == "poisson") {
-#     out <- poisl0(x, y, lam, maxit, eps)
-#   }
+  
   out <- l0araC(x, y, family, lam, maxit, eps)
   # output
   res <- list(beta = drop(out$beta), df = sum(out$beta!=0), lambda = lam, iter = out$iter, family = family, x = x, y = y)
